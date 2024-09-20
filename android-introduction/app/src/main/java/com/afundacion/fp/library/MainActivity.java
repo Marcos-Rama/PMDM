ff550653b077ff06feb1d81b7c2bd474c3eb89aa
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2024-09-20 10:55:54 +0200

    Tarea #9, Click botón sin clase nueva

--- android-introduction/app/src/main/java/com/afundacion/fp/library/MainActivity.java
+++ android-introduction/app/src/main/java/com/afundacion/fp/library/MainActivity.java
@@ -3,11 +3,14 @@ package com.afundacion.fp.library;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Notification;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
         ActionHandler myHandler = new ActionHandler(this);
         Button myButton = findViewById(R.id.toastButton);
         myButton.setOnClickListener(myHandler);
+        Button otherButton = findViewById(R.id.bottomButton);
+        otherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "Mostrando otra actividad...", Toast.LENGTH_LONG).show();
 
+            }
+        });
     }
 }
