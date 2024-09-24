0eec624654d14871248c6a33f1a4daee02fd6307
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2024-09-24 10:22:19 +0200

    Tarea #16, primer fragmento

--- android-introduction/app/src/main/java/com/afundacion/fp/library/MonstersActivity.java
+++ android-introduction/app/src/main/java/com/afundacion/fp/library/MonstersActivity.java
@@ -8,6 +8,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -20,12 +21,15 @@ public class MonstersActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monsters);
+        Fragment myFragment = new Fragment1();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new Fragment1()).commit();
         BottomNavigationView bar = findViewById(R.id.bottomNavigation);
         bar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if (item.getItemId() == R.id.item1) {
-                    Toast.makeText(context,"Has clicado en Digimon 1",Toast.LENGTH_SHORT).show();
+                    Fragment myFragment = new Fragment1();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, myFragment).commit();
                 }
                 if (item.getItemId() == R.id.item2) {
                     Toast.makeText(context,"Has clicado en Digimon 2",Toast.LENGTH_SHORT).show();
