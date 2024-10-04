0093c9cd8a21debf0f77cdce8661f0a2491e2a5b
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2024-10-04 13:07:27 +0200

    Tarea 32, click en recycleview

--- android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/ClipViewHolder.java
+++ android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/ClipViewHolder.java
@@ -1,20 +1,35 @@
 package com.afundacion.fp.clips;
 
+import android.content.Context;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ClipViewHolder extends RecyclerView.ViewHolder {
     private TextView textView;
+    private Clip clip;
 
     public ClipViewHolder(@NonNull View itemView) {
         super(itemView);
         textView = itemView.findViewById(R.id.texto1);
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int clipId = clip.getId();
+                Context context = view.getContext();
+                Toast.makeText(context,"Clicked on cell with clipId: " + clipId , Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
+
     public void bindClip(Clip clip) {
         this.textView.setText(clip.getTitle());
+        this.clip = clip;
     }
 
 }
