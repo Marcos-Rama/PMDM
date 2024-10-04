dabde2ac4b441f068fdb856ce6ff28daa08d525c
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2024-10-04 12:40:26 +0200

    Tarea 30, crear clase para recycleview

---
+++ android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/ClipViewHolder.java
@@ -0,0 +1,18 @@
+package com.afundacion.fp.clips;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ClipViewHolder extends RecyclerView.ViewHolder {
+    private TextView textView;
+
+    public ClipViewHolder(@NonNull View itemView) {
+        super(itemView);
+        textView = itemView.findViewById(R.id.texto1);
+    }
+    
+}
+
