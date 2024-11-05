ece384d6f3cbcc75bd3ef521e23d16db84f1d493
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2024-11-05 12:22:40 +0100

    continuación de animaciones

--- godot/Juego_Godot/raspano/scenes/main_character.gd
+++ godot/Juego_Godot/raspano/scenes/main_character.gd
@@ -6,20 +6,28 @@ const JUMP_VELOCITY = -400.0
 @onready var animation_main_char = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	#Animations
-	if (velocity.x > 1 || velocity.x < -1):
-		animation_main_char.animation = "run"
+	
+	if is_on_floor():
+		if velocity.x > 1 or velocity.x < -1:
+			animation_main_char.animation = "run"
+		else:
+			animation_main_char.animation = "idle"
+	elif Input.is_action_pressed("Jump"):
+		velocity.y = JUMP_VELOCITY
+		animation_main_char.animation = "jump_up"
+			
 	else:
-		animation_main_char.animation = "idle"
-	# Add the gravity.
+		if velocity.y > 0.0:
+			animation_main_char.animation = "jump_down"
+		else:
+			animation_main_char.animation = "jump_up"
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		animation_main_char.animation = "Jump"
 		
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+
 		
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
