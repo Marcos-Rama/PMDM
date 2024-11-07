4a9d57a47616c96887152d594965afc2b125fd93
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2024-11-07 16:58:08 +0100

    Añadidos primeros enemigos, de momento estáticos y no hacen daño

--- godot/Juego_Godot/raspano/scenes/main_character.gd
+++ godot/Juego_Godot/raspano/scenes/main_character.gd
@@ -48,7 +48,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	# Movimiento lateral
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
