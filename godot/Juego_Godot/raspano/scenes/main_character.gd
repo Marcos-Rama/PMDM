2d19b24eab44c5789857730006017a215b94d538
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2024-11-07 09:42:48 +0100

    Perfeccionado movimiento del personaje, establecida camara que siga al personaje, creación de coleccionable (pendiente de ponerlos todos cuando termine el nivel), cambio de nivel al alcanzar el objetivo

--- godot/Juego_Godot/raspano/scenes/main_character.gd
+++ godot/Juego_Godot/raspano/scenes/main_character.gd
@@ -3,33 +3,46 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const MAX_JUMP_TIME = 0.2  # Tiempo máximo de salto en segundos
 @onready var animation_main_char = $AnimatedSprite2D
 
+var jump_time = 0.0 #Almacenar el tiempo que lleva en el aire
+var is_jumping = false
+
+# Define la posición inicial del personaje con un valor por defecto
+var initial_position: Vector2 = Vector2.ZERO
+
+ # Cambia a la posición inicial deseada
+
 func _physics_process(delta: float) -> void:
+	initial_position = position
 	
 	if is_on_floor():
+		is_jumping = false  # Reinicia el estado de salto al tocar el suelo
+		jump_time = 0.0  # Reinicia el tiempo de salto
 		if velocity.x > 1 or velocity.x < -1:
 			animation_main_char.animation = "run"
 		else:
 			animation_main_char.animation = "idle"
-	elif Input.is_action_pressed("Jump"):
-		velocity.y = JUMP_VELOCITY
-		animation_main_char.animation = "jump_up"
-			
 	else:
+		# Cambia la animación según si sube o baja en el aire
 		if velocity.y > 0.0:
 			animation_main_char.animation = "jump_down"
 		else:
 			animation_main_char.animation = "jump_up"
+			
+	#Manejo del salto
+	if Input.is_action_pressed("Jump") and (is_on_floor() or is_jumping):
+		if jump_time < MAX_JUMP_TIME:
+			velocity.y = JUMP_VELOCITY
+			jump_time += delta
+			is_jumping = true  # Indica que el personaje está en modo de salto
+	elif not Input.is_action_pressed("Jump") and is_jumping:
+		is_jumping = false  # Termina el salto cuando se deja de presionar la tecla
+			
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-	# Handle jump.
-
-		
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movimiento lateral
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -40,3 +53,10 @@ func _physics_process(delta: float) -> void:
 	
 	var isLeft = velocity.x < 0
 	animation_main_char.flip_h = isLeft
+
+func respawn():
+	get_tree().reload_current_scene()
+	
+func _on_water_1_body_entered(body: Node2D) -> void:
+	if "Main_char" in body.name:
+		body.respawn()# Replace with function body.
