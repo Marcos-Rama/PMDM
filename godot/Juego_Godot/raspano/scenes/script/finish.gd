edd7c537086e721bd89556f38cc49e8930f76a85
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2024-11-12 15:35:41 +0100

    Arreglado que los puntos no se reiniciaran al acabar la partida, para empezar otra

--- godot/Juego_Godot/raspano/scenes/script/finish.gd
+++ godot/Juego_Godot/raspano/scenes/script/finish.gd
@@ -4,6 +4,16 @@ extends Area2D
 @onready var game_manager = %Gamemanager
 
 func _on_body_entered(body: Node2D) -> void:
-	if (body.name == "Main_char") and game_manager.points >= 3:
-		get_tree().change_scene_to_packed(target_level)
+	if Global.total_scenes == 0:
+		if (body.name == "Main_char") and game_manager.points >= 3:
+			get_tree().change_scene_to_packed(target_level)
+			Global.total_scenes += 1
+			if Global.total_scenes == 1:
+				Global.total_scenes = 0
+				game_manager.points = 0
+				game_manager.lives = 3
+	#elif Global.total_scenes == 1:
+		#if (body.name == "Main_char") and game_manager.points >= 6:
+			#get_tree().change_scene_to_packed(target_level)
+			#Global.total_scenes += 1
 		
