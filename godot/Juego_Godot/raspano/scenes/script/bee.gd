0da4b6cf54709a37dd3588c292d56f515e77d09c
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2024-11-13 18:49:56 +0100

    Creado el scrip del jabalí para que embista si te detecta

--- godot/Juego_Godot/raspano/scenes/script/bee.gd
+++ godot/Juego_Godot/raspano/scenes/script/bee.gd
@@ -5,11 +5,11 @@ const speed_aggro = 100
 var dir: Vector2
 var player: CharacterBody2D
 
-var is_bat_chase: bool
+var is_bee_chase: bool
 @onready var game_manager = %Gamemanager
 
 func _ready():
-	is_bat_chase = false
+	is_bee_chase = false
 	
 func _process(delta):
 	move(delta)
@@ -17,24 +17,24 @@ func _process(delta):
 	
 
 func move(delta):
-	if is_bat_chase:
+	if is_bee_chase:
 		player = Global.playerBody
 		velocity = position.direction_to(player.position) * speed_aggro
 		dir.x = abs(velocity.x)/velocity.x
-	elif !is_bat_chase:
+	elif !is_bee_chase:
 		velocity += dir * speed * delta
 	move_and_slide()
 	
 
 func _on_timer_timeout() -> void:
 	$Timer.wait_time = choose([0.5,0.8])
-	if !is_bat_chase:
+	if !is_bee_chase:
 		dir = choose([Vector2.RIGHT, Vector2.UP, Vector2.LEFT, Vector2.DOWN])
 		print(dir)
 
 func handle_animation():
 	var animated_sprite = $AnimatedSprite2D
-	animated_sprite.play("fly")
+	animated_sprite.play("walk")
 	if dir.x == 1:
 		animated_sprite.flip_h = true
 	elif dir.x == -1:
@@ -47,11 +47,11 @@ func choose(array):
 
 func _on_bee_aggro_body_entered(body: Node2D) -> void:
 	if body.name == "Main_char":
-		is_bat_chase = true
+		is_bee_chase = true
 		
 func _on_bee_aggro_body_exited(body: Node2D) -> void:
 	if body.name == "Main_char":
-		is_bat_chase = false
+		is_bee_chase = false
 
 
 func die():
