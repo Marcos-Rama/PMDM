bef1f19ec124e06f93b951272573f6868652dd1f
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2024-11-15 10:50:46 +0100

    Creación de escena que será el menu de opciones, con el guardado de ventana funcionando (comprobando) y presupuestamente el control de volumen tambien(falta probarlo)

---
+++ godot/Juego_Godot/raspano/scenes/script/Persistence.gd
@@ -0,0 +1,32 @@
+extends Node
+
+const PATH = "user://settings.cfg"
+var config = ConfigFile.new()
+
+func _ready():
+	for action in InputMap.get_actions():
+		if InputMap.action_get_events(action).size() != 0:
+			config.set_value("Controls",action,InputMap.action_get_events(action)[0])
+	
+	config.set_value("Video", "fullscreen", DisplayServer.WINDOW_MODE_WINDOWED)
+	config.set_value("Video", "borderless", false)
+	
+	load_data()
+
+func save_data():
+	config.save(PATH)
+
+func load_data():
+	if config.load("user://settings.cfg") != OK:
+		save_data()
+		return
+		
+	load_video_settings()
+
+func load_video_settings():
+	var screen_type = config.get_value("Video","fullscreen")
+	DisplayServer.window_set_mode(screen_type)
+ 
+	var borderless = config.get_value("Video","borderless")
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, borderless)
+ 
